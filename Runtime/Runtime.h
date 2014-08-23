5fe4f2d32708c43cde0daeb60ff77d5c75c181b8
Paul Young <paulyoungonline@gmail.com>
2014-08-23 14:26:12 -0400

    Initial Commit

---
+++ Runtime/Runtime.h
@@ -0,0 +1,19 @@
+//
+//  Runtime.h
+//  Runtime
+//
+//  Created by Paul Young on 23/08/2014.
+//  Copyright (c) 2014 CocoaFlow. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Runtime.
+FOUNDATION_EXPORT double RuntimeVersionNumber;
+
+//! Project version string for Runtime.
+FOUNDATION_EXPORT const unsigned char RuntimeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Runtime/PublicHeader.h>
+
+
