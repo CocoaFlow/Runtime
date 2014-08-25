1f21deb7abe20221c8ba44d33aed606226414cf4
Paul Young <paulyoungonline@gmail.com>
2014-08-24 22:53:28 -0400

    Reconfigure targets and schemes to support both Mac and iOS.

--- Runtime/Runtime.h
+++ Runtime/Runtime.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 CocoaFlow. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Runtime.
 FOUNDATION_EXPORT double RuntimeVersionNumber;
