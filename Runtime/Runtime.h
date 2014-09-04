ed11857a94401fe2bb5757e4238ace6b16222c28
Paul Young <paulyoungonline@gmail.com>
2014-09-04 01:04:46 -0400

    Remove whitespace.

--- Runtime/Runtime.h
+++ Runtime/Runtime.h
@@ -15,5 +15,3 @@ FOUNDATION_EXPORT double RuntimeVersionNumber;
 FOUNDATION_EXPORT const unsigned char RuntimeVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Runtime/PublicHeader.h>
-
-
